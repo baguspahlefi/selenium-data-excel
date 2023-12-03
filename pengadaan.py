26eaf205ff1260823756c4a3cfbf9c9a823b3ec2
Bagus Levi <baguspahlefi200@gmail.com>
2023-12-03 23:14:28 +0700

    update pengadaan 2

--- pengadaan.py
+++ pengadaan.py
@@ -13,10 +13,11 @@ import time
 wb = load_workbook(filename="data-ikan.xlsx")
 
 sheetRange = wb['Sheet1']
+sheetRangeSpek = wb['Sheet2']
 
 driver = webdriver.Firefox()
 
-driver.get("https://e-katalog.lkpp.go.id/v3/katalog/produk/create/76103719") 
+driver.get("https://e-katalog.lkpp.go.id/v3/katalog/produk/create/76105689") 
 actions = ActionChains(driver)
 driver.maximize_window()
 driver.implicitly_wait(10)
@@ -49,10 +50,87 @@ try:
         ).click()
 
         if kategori == "Mistar Muka Air":
-            
+            search_kategori = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, " //input[@role='textbox']"))
+            )
+            search_kategori.send_keys('Mistar Muka Air')
+            actions.key_down(Keys.ENTER).key_up(Keys.ENTER).perform()
+            time.sleep(2)
+
             WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//textarea[@name='attr-id-17685649']"))
             ).send_keys('asu')
+
+        elif kategori == "Struktur Baja" :
+            search_kategori = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, " //input[@role='textbox']"))
+            )
+            search_kategori.send_keys('Struktur Baja')
+            actions.key_down(Keys.ENTER).key_up(Keys.ENTER).perform()
+            time.sleep(2)
+            j = 2
+            while j <= len(sheetRangeSpek['B']):
+                keteranganLainya = sheetRangeSpek['B' + str(i)].value
+                TKDN = sheetRangeSpek['C' + str(i)].value
+                perusahaan = sheetRangeSpek['D' + str(i)].value
+                lokasi = sheetRangeSpek['E' + str(i)].value
+                KBLI = sheetRangeSpek['F' + str(i)].value
+
+                WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//textarea[@name='attr-id-17685464']"))
+                ).send_keys(keteranganLainya)
+
+                WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//textarea[@name='attr-id-17685465']"))
+                ).send_keys(TKDN)
+
+                WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//textarea[@name='attr-id-17685466']"))
+                ).send_keys(perusahaan) 
+
+                WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//textarea[@name='attr-id-17685467']")) 
+                ).send_keys(lokasi) 
+
+                WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//textarea[@name='attr-id-17685468']"))
+                ).send_keys(KBLI)
+
+                j = i+1
+
+           
+
+        elif kategori == "Pengadaan Barang Peralatan/Mesin/Perlengkapan Budidaya" :
+            search_kategori = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, " //input[@role='textbox']"))
+            )
+            search_kategori.send_keys('Pengadaan Barang')
+            actions.key_down(Keys.ENTER).key_up(Keys.ENTER).perform()
+            time.sleep(2)
+
+            WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//textarea[@name='attr-id-17685037']"))
+            ).send_keys('pengadaan mesin')
+
+        elif kategori == "Pengadaan Barang Sarana dan Prasana Laboratorium Perikanan Budidaya" :
+            search_kategori = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, " //input[@role='textbox']"))
+            )
+            search_kategori.send_keys('Pengadaan Barang')
+            actions.key_down(Keys.ARROW_DOWN).key_up(Keys.ARROW_DOWN).perform()
+            actions.key_down(Keys.ENTER).key_up(Keys.ENTER).perform()
+            time.sleep(2)
+
+            WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//textarea[@name='attr-id-17685076']"))
+            ).send_keys('Pengadaan Barang Sarana dan Prasana Laboratorium Perikanan Budidaya')
+
+        
+
+        
+        
+
+        
 
 
         
